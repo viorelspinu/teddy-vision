b056f3f9b40d17fe46b8f3e3f56c2f1241dcb93a
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-28 16:23:11 +0300

    u

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo apt-get -yq install python-pip
 sudo apt-get -yq install python-picamera python3-picamera
 sudo apt-get -yq install  python-requests
 sudo apt-get -yq install  python-simplejson
+sudo apt-get -yq install  python-pygame
