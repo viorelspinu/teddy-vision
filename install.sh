a5fbd48d604fe271862f8f0dd4d54a20fd71179d
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-28 16:04:31 +0300

    update

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
 sudo apt-get update
 sudo apt-get -yq install git
+sudo apt-get -yq install python-pip
 sudo apt-get -yq install python-picamera python3-picamera
+sudo apt-get -yq install  python-requests
+
