b56eabe6e3b053cd47783e56753d39eab386a5cf
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-29 15:12:13 +0300

    u

--- push_to_talk.sh
+++ push_to_talk.sh
@@ -2,8 +2,8 @@
 python ./record_respearker.py
 ffmpeg -i ./hello.wav -ar 11025 -ac 2 output.wav -y
 aplay ./wav/one_sec.wav
-export PROJECT_ID=teddy-8b681
-export DEVICE_ID=teddy-8b681-teddy-google-assistant-dlmtw8
+export PROJECT_ID=teddy-assistant
+export DEVICE_ID=teddy-assistant-teddy-o07wiz
 googlesamples-assistant-pushtotalk --project-id $PROJECT_ID --device-model-id $DEVICE_ID --input-audio-file ./output.wav --output-audio-file ./response.wav
 aplay ./response.wav
 sleep 0.5
