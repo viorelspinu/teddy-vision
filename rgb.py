e9001d9e7133118f4c4c2c33dd833b144a0fa47b
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-29 14:03:56 +0300

    u

--- rgb.py
+++ rgb.py
@@ -8,12 +8,12 @@ class RGBLed:
     def __init__(self):
         GPIO.setmode(GPIO.BCM)
         GPIO.setwarnings(False)
-        GPIO.setup(18, GPIO.OUT)
+        GPIO.setup(25, GPIO.OUT)
         GPIO.setup(23, GPIO.OUT)
         GPIO.setup(24, GPIO.OUT)
 
     def set_color(self, r, g, b):      
         GPIO.output(23, r)
         GPIO.output(24, g)
-        GPIO.output(18, b)
+        GPIO.output(25, b)
         
