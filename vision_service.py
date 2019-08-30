fd00ba78303849eee13ef65fcbc3ca55f266f61a
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 16:33:25 +0300

    u

--- vision_service.py
+++ vision_service.py
@@ -1,8 +1,8 @@
 import os
 from local_communication_service import LocalCommunicationService as local_communication_service
 from camera_service import CameraService
-from sound_processor import SoundProcessor
-from cloud_processor import CloudProcessor
+from sound_service import SoundService
+from cloud_service import CloudService
 from rgb import RGBLed
 
 
@@ -10,11 +10,11 @@ class VisionService:
 
     def do_vision(self):
         camera_service = CameraService()
-        cloud_processor = CloudProcessor()
+        cloud_processor = CloudService()
         led = RGBLed()
         led.set_color(0, 1, 0)
-        SoundProcessor.getInstance().play("./wav/keep_it_still.wav", True)
+        SoundService.getInstance().play("./wav/keep_it_still.wav", True)
         camera_service.take_photo("photo.jpg")
         led.set_color(1, 0, 0)
-        SoundProcessor.getInstance().play("./wav/done_have_to_think.wav", True)
+        SoundService.getInstance().play("./wav/done_have_to_think.wav", True)
         cloud_processor.process_photo("photo.jpg")
