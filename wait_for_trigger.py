a65d0dd78b43879663b5696dd611a48e651a12d0
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 17:38:27 +0300

    u

--- wait_for_trigger.py
+++ wait_for_trigger.py
@@ -50,7 +50,9 @@ class WaitForTriggerService:
             distance_diff = abs(distance - self.__old_distance)
             
             if (distance_diff < 50):
-                self.__distance_variantion = 0
+                self.__distance_variantion = self.__distance_variantion - 50
+                if (self.__distance_variantion < 0):
+                    self.__distance_variantion = 0
             else:
                 self.__distance_variantion = self.__distance_variantion + distance_diff
             self.__old_distance = distance
