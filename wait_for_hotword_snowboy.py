554cd6edc41e4e1f4f9f08a6ee466f49908dd0aa
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 10:40:26 +0300

    u

--- wait_for_hotword_snowboy.py
+++ wait_for_hotword_snowboy.py
@@ -29,7 +29,7 @@ models = ["./t.model", "./explore.mdl"]
 
 detector = snowboydecoder.HotwordDetector(models, sensitivity=0.5, audio_gain=1)
 
-callbacks = [lambda: detected_teddy,
-             lambda: detected_explore]
+callbacks = [detected_teddy,
+             detected_explore]
 
 detector.start(detected_callback=callbacks, interrupt_check=interrupt_callback)
