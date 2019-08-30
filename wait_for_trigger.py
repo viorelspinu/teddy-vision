28e83d4bd8b9e1b415026921ed80ae1a43100816
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 16:11:57 +0300

    u

--- wait_for_trigger.py
+++ wait_for_trigger.py
@@ -1,12 +1,10 @@
 
+import os
+import snowboydecoder
+from local_communication_service import LocalCommunicationService as local_communication_service
+from sonar_service import SonarService
 import sys
 sys.path.append('./snowboy')
-
-
-from sonar_service import SonarService
-from local_communication_service import LocalCommunicationService as local_communication_service
-import snowboydecoder
-import os
 
 
 class WaitForTriggerService:
@@ -15,32 +13,30 @@ class WaitForTriggerService:
     def detected_teddy(self):
         print "Teddy Detected"
         local_communication_service.getInstance().write_hotword("teddy")
-        terminate_detector()
+        self.terminate_detector()
 
     def detected_explore(self):
         print "Explore Detected"
         local_communication_service.getInstance().write_hotword("explore")
-        terminate_detector()
+        self.terminate_detector()
 
     def terminate_detector(self):
-        detector.terminate()
-        global interrupted
-        interrupted = True
+        self.interrupted = True
+        self.detector.terminate()
 
     def interrupt_callback(self):
-        global interrupted
-        return interrupted
+        return self.interrupted
 
     def main(self):
         sonar_service = SonarService.getInstance()
 
         models = ["./snowboy_models/teddy.mdl", "./snowboy_models/explore.mdl"]
-        detector = snowboydecoder.HotwordDetector(models, sensitivity=0.5, audio_gain=1)
+        self.detector = snowboydecoder.HotwordDetector(models, sensitivity=0.5, audio_gain=1)
 
         callbacks = [detected_teddy,
                      detected_explore]
 
-        detector.start(detected_callback=callbacks, interrupt_check=interrupt_callback)
+        detector.start(detected_callback=callbacks, interrupt_check=self.interrupt_callback)
 
 
 wait_for_trigger_service = WaitForTriggerService()
