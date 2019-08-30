aa9702288215c87361dbfe7c66bb1b11abdc90ef
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 12:36:23 +0300

    u

--- run_action_on_hotword.py
+++ run_action_on_hotword.py
@@ -1,26 +1,14 @@
 import os
 from local_communication_service import LocalCommunicationService as local_communication_service
-#from camera import Camera
-from sound_processor import SoundProcessor
-#from cloud_processor import CloudProcessor
-#from rgb import RGBLed
+from vision_service import VisionService as vision_service
 
 
 hotword = local_communication_service.getInstance().read_hotword()
 
 
 if ("teddy" == hotword):
-    SoundProcessor.getInstance().play("./wav/yes.wav", True)
-    #os.system("aplay ./wav/yes.wav")
+    os.system("aplay ./wav/yes.wav")
     os.system("./push_to_talk.sh")
 
 if ("explore" == hotword):
-    camera = Camera()
-    cloud_processor = CloudProcessor()
-    led = RGBLed()
-    led.set_color(0, 1, 0)
-    SoundProcessor.getInstance().play("./wav/keep_it_still.wav", True)
-    camera.take_photo()
-    led.set_color(1, 0, 0)
-    SoundProcessor.getInstance().play("./wav/done_have_to_think.wav", True)
-    cloud_processor.process_photo()
+    vision_service.getInstance().do_vision()
---
+++ vision_service.py
@@ -0,0 +1,30 @@
+import os
+from local_communication_service import LocalCommunicationService as local_communication_service
+from camera import Camera
+from sound_processor import SoundProcessor
+from cloud_processor import CloudProcessor
+from rgb import RGBLed
+
+
+class VisionService:
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        if VisionService.__instance == None:
+            VisionService()
+        return VisionService.__instance
+
+    def __init__(self):
+        VisionService.__instance = self
+
+    def do_vision(self):
+        camera = Camera()
+        cloud_processor = CloudProcessor()
+        led = RGBLed()
+        led.set_color(0, 1, 0)
+        SoundProcessor.getInstance().play("./wav/keep_it_still.wav", True)
+        camera.take_photo()
+        led.set_color(1, 0, 0)
+        SoundProcessor.getInstance().play("./wav/done_have_to_think.wav", True)
+        cloud_processor.process_photo()
