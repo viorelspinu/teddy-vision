e9a7ad0d2e4f83430df126d97611879ce2bf2f2a
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 18:44:12 +0300

    u

--- push_to_talk.sh
+++ push_to_talk.sh
@@ -6,6 +6,7 @@ ffmpeg -i ./hello.wav -ar 11025 -ac 2 output.wav -y
 aplay ./wav/one_sec.wav
 export PROJECT_ID=teddy-assistant
 export DEVICE_ID=teddy-assistant-teddy-o07wiz
-googlesamples-assistant-pushtotalk --project-id $PROJECT_ID --device-model-id $DEVICE_ID --input-audio-file ./output.wav --output-audio-file ./response.wav
-aplay ./response.wav
-sleep 0.5
+googlesamples-assistant-pushtotalk --project-id $PROJECT_ID --device-model-id $DEVICE_ID --input-audio-file ./output.wav --output-audio-file ./assistant_response.wav
+ffmpeg -i assistant_response.wav -filter:a "volume=7dB" assistant_response_loud.wav
+aplay ./assistant_response_loud.wav
+sleep 0.1
