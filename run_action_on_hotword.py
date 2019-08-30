8e5d2d5e8727431189e0dd706cf5b958bcfec3ff
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 11:38:04 +0300

    u

--- run_action_on_hotword.py
+++ run_action_on_hotword.py
@@ -1,4 +1,25 @@
+import os
 from local_communication_service import LocalCommunicationService as local_communication_service
+from camera import Camera
+from sound_processor import SoundProcessor
+from cloud_processor import CloudProcessor
+from rgb import RGBLed
+from sonar import Sonar
+import time
 
 hotword = local_communication_service.getInstance().read_hotword()
-print(hotword)
+
+camera = Camera()
+cloud_processor = CloudProcessor()
+led = RGBLed()
+
+if ("teddy" == hotword):
+    os.system("./push_to_talk.sh")
+
+if ("explore" == hotword):
+    led.set_color(0, 1, 0)
+    SoundProcessor.getInstance().play("./wav/keep_it_still.wav", True)
+    camera.take_photo()
+    led.set_color(1, 0, 0)
+    SoundProcessor.getInstance().play("./wav/done_have_to_think.wav", True)
+    cloud_processor.process_photo()
