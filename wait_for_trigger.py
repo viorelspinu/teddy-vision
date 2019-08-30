33b6a23c8e66a4b1b79385c9205f53cfa34143d5
Viorel Spinu <viorel.spinu@gmail.com>
2019-08-30 16:12:18 +0300

    u

--- wait_for_trigger.py
+++ wait_for_trigger.py
@@ -1,10 +1,12 @@
 
+import sys
+sys.path.append('./snowboy')
+
 import os
 import snowboydecoder
 from local_communication_service import LocalCommunicationService as local_communication_service
 from sonar_service import SonarService
-import sys
-sys.path.append('./snowboy')
+
 
 
 class WaitForTriggerService:
