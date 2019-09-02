ad51e7bdffc3e0d98a93bc67904e544849fa7857
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-02 15:53:03 +0300

    u

--- camera_service.py
+++ camera_service.py
@@ -13,21 +13,12 @@ class CameraService:
 
     def __init__(self):
         self.camera = PiCamera()
-        self.camera.iso = 100
-        # Wait for the automatic gain control to settle
-        sleep(2)
-        # Now fix the values
-        self.camera.shutter_speed = self.camera.exposure_speed
-        self.camera.exposure_mode = 'off'
-        g = self.camera.awb_gains
-        self.camera.awb_mode = 'off'
-        self.camera.awb_gains = g
         CameraService.__instance = self
 
     def take_photo(self, file_path):
         print("PHOTO START")
         self.camera.resolution = (1024, 768)
         self.camera.start_preview()
-        sleep(1)
+        sleep(0.5)
         self.camera.capture(file_path)
         print("PHOTO DONE")
