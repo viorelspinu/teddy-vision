2466a4cdc4b696a2bf6c20d5628d411be1798fc9
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-04 18:38:51 +0300

    u

--- run_assistant_instance.sh
+++ run_assistant_instance.sh
@@ -5,6 +5,14 @@ python ./record_audio_service.py
 
 if [ $? -eq 0 ]
 then
+  echo "Successfully executed script"
+else
+  # Redirect stdout from echo command to stderr.
+  echo "Script exited with error." >&2
+fi
+
+if [ $? -eq 0 ]
+then
     exit 1
 fi
 ffmpeg -i ./hello.wav -ar 11025 -ac 2 output.wav -y > /dev/null 2>&1 < /dev/null
