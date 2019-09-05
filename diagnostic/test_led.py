6c2950abecbb0db0a6436a885931acdbe0ee5523
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-05 17:41:22 +0300

    u

---
+++ diagnostic/test_led.py
@@ -0,0 +1,15 @@
+
+import sys
+sys.path.append('./snowboy')
+
+from rgb_led_service import RGBLedService
+from time import sleep
+
+RGBLedService.getInstance().set_color(1, 0, 0)
+sleep(0.5)
+RGBLedService.getInstance().set_color(0, 1, 0)
+sleep(0.5)
+RGBLedService.getInstance().set_color(0, 0, 1)
+sleep(0.5)
+
+
