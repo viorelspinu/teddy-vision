823a19a993b2ef2cd2f19d14cdcbe940ed926462
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-11 15:00:54 +0300

    u

--- webserver/start_server.py
+++ webserver/start_server.py
@@ -1,3 +1,4 @@
+from cloud_service import CloudService
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -5,7 +6,6 @@ from flask import request
 import sys
 import os
 sys.path.append('./..')
-from cloud_service import CloudService
 
 cloud_service = CloudService()
 
@@ -13,9 +13,23 @@ cloud_service = CloudService()
 app = Flask(__name__)
 
 
+def get_lang_codes():
+    lang_en = {}
+    lang_en.code = CloudService.TTS_LANGUAGE_CODE_ENGLISH
+    lang_en.name = "English"
+    lang_fr = {}
+    lang_fr.code = CloudService.TTS_LANGUAGE_CODE_FRENCH
+    lang_fr.name = "French"
+    lang_de = {}
+    lang_de.code = CloudService.TTS_LANGUAGE_CODE_GERMAN
+    lang_de.name = "German"
+
+    return [lang_en, lang_fr, lang_de]
+
+
 @app.route('/')
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', lang_codes=get_lang_codes())
 
 
 @app.route('/speak', methods=['POST'])
@@ -27,8 +41,9 @@ def speak():
     cloud_service.decode_text_to_file_as_base64(mp3_base64, "out.mp3")
     os.system("ffmpeg -i ./out.mp3 out.wav -y > /dev/null 2>&1 < /dev/null")
     os.system("aplay ./out.wav")
-    
-    return render_template('home.html')
+
+    return render_template('home.html', lang_codes=get_lang_codes())
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
