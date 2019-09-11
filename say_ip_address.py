dc38cc76e7c286f7050ed1d5f3f6eca1b381e5ad
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-11 16:50:16 +0300

    u

--- say_ip_address.py
+++ say_ip_address.py
@@ -2,6 +2,7 @@ import sys
 import os
 import socket
 from cloud_service import CloudService
+from voice_codes_constants import *
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -13,7 +14,7 @@ s.close()
 
 cloud_service = CloudService()
 input_text = "<speak>my ip address is" + ip_address.replace(".", "<break time=\"0.5s\"/>") + "</speak>"
-mp3_base64 = cloud_service.do_text_to_speech_post(input_text, CloudService.TTS_LANGUAGE_CODE_ENGLISH, CloudService.TTS_VOICE_CODE_ENGLISH, ssml=True)
+mp3_base64 = cloud_service.do_text_to_speech_post(input_text, TTS_LANGUAGE_CODE_ENGLISH, TTS_VOICE_CODE_ENGLISH, ssml=True)
 cloud_service.decode_text_to_file_as_base64(mp3_base64, "out.mp3")
 os.system("ffmpeg -i ./out.mp3 out.wav -y > /dev/null 2>&1 < /dev/null")
 os.system("aplay ./out.wav")
