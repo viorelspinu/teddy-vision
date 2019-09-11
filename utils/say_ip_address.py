934d7fa1812b223fd5ec6a4380fb1733605877b4
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-11 13:52:11 +0300

    u

--- utils/say_ip_address.py
+++ utils/say_ip_address.py
@@ -15,7 +15,7 @@ s.close()
 
 
 cloud_service = CloudService()
-input_text = "<speak>my ip address is" + ip_address.replace(".", "<break time=\"1s\"/>") + "</speak>"
+input_text = "<speak>my ip address is" + ip_address.replace(".", "<break time=\"0.5s\"/>") + "</speak>"
 mp3_base64 = cloud_service.do_text_to_speech_post(input_text, CloudService.TTS_LANGUAGE_CODE_ENGLISH, CloudService.TTS_VOICE_CODE_ENGLISH, ssml=True)
 cloud_service.decode_text_to_file_as_base64(mp3_base64, "out.mp3")
 os.system("ffmpeg -i ./out.mp3 out.wav -y > /dev/null 2>&1 < /dev/null")
