f117e9a5d14b95a71a5b87cad8739f0afdad50c4
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-11 15:11:33 +0300

    u

--- webserver/start_server.py
+++ webserver/start_server.py
@@ -1,4 +1,5 @@
 
+from cloud_service import CloudService
 from flask import Flask
 from flask import render_template
 from flask import request
@@ -6,46 +7,58 @@ from flask import request
 import sys
 import os
 sys.path.append('./..')
-from cloud_service import CloudService
 
 cloud_service = CloudService()
 
 
 app = Flask(__name__)
 
+lang_codes = get_lang_codes()
+
 
 def get_lang_codes():
     lang_en = {}
-    lang_en['code'] = CloudService.TTS_LANGUAGE_CODE_ENGLISH
+    lang_en['lang_code'] = CloudService.TTS_LANGUAGE_CODE_ENGLISH
+    lang_de['voice_code'] = CloudService.TTS_VOICE_CODE_ENGLISH
     lang_en['name'] = "English"
+
     lang_fr = {}
-    lang_fr['code'] = CloudService.TTS_LANGUAGE_CODE_FRENCH
+    lang_fr['lang_code'] = CloudService.TTS_LANGUAGE_CODE_FRENCH
+    lang_de['voice_code'] = CloudService.TTS_VOICE_CODE_FRENCH
     lang_fr['name'] = "French"
+
     lang_de = {}
-    lang_de['code'] = CloudService.TTS_LANGUAGE_CODE_GERMAN
+    lang_de['lang_code'] = CloudService.TTS_LANGUAGE_CODE_GERMAN
+    lang_de['voice_code'] = CloudService.TTS_VOICE_CODE_GERMAN
     lang_de['name'] = "German"
 
     return [lang_en, lang_fr, lang_de]
 
+def get_lang(lang_code):
+    for lang in lang_codes:
+        if (lang['lang_code'] == lang_code):
+            return lang
+
 
 @app.route('/')
 def hello_world():
-    return render_template('home.html', lang_codes=get_lang_codes())
+    return render_template('home.html', lang_codes=lang_codes)
 
 
 @app.route('/speak', methods=['POST'])
 def speak():
     text = request.form['text_to_speak']
     lang_code = request.form['lang_code']
+    lang = get_lang(lang_code)
     print(lang_code)
     print(text)
     input_text = text
-    mp3_base64 = cloud_service.do_text_to_speech_post(input_text, CloudService.TTS_LANGUAGE_CODE_ENGLISH, lang_code)
+    mp3_base64 = cloud_service.do_text_to_speech_post(input_text, lang['lang_code'], lang['voice_code'])
     cloud_service.decode_text_to_file_as_base64(mp3_base64, "out.mp3")
     os.system("ffmpeg -i ./out.mp3 out.wav -y > /dev/null 2>&1 < /dev/null")
     os.system("aplay ./out.wav")
 
-    return render_template('home.html', lang_codes=get_lang_codes())
+    return render_template('home.html', lang_codes=lang_codes)
 
 
 if __name__ == "__main__":
