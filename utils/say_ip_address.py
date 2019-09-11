ad31d8db4f2ebe28f7ad3a49cb0ab365eb941dd6
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-11 13:43:05 +0300

    u

---
+++ utils/say_ip_address.py
@@ -0,0 +1,23 @@
+import sys
+import os
+sys.path.append('./..')
+import socket
+
+from cloud_service import CloudService
+
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(("8.8.8.8", 80))
+ip_address = s.getsockname()[0]
+print(ip_address)
+s.close()
+
+
+cloud_service = CloudService()
+input_text = ip_address
+mp3_base64 = cloud_service.do_text_to_speech_post(input_text, CloudService.TTS_LANGUAGE_CODE_ENGLISH, CloudService.TTS_VOICE_CODE_ENGLISH)
+cloud_service.decode_text_to_file_as_base64(mp3_base64, "out.mp3")
+os.system("ffmpeg -i ./out.mp3 out.wav -y > /dev/null 2>&1 < /dev/null")
+os.system("aplay ./out.wav")
+print("file saved as 'out.wav'")
