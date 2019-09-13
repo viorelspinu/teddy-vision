25e12a5da2703f4654c2d60109e8e883a56df778
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-13 18:37:44 +0300

    u

--- install/2_install_dependencies.sh
+++ install/2_install_dependencies.sh
@@ -9,4 +9,5 @@ sudo apt-get -yq install  python-pyaudio
 sudo apt-get -yq install python-pyaudio python3-pyaudio sox
 sudo apt-get -yq install python-pyaudio python-pip libatlas-base-dev portaudio19-dev
 sudo apt-get -yq install  python-flask
+pip install  lomond --user
 pip install pyaudio --user
