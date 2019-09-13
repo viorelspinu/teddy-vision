86c1d0473845427fb5a55353c57a748c69e1ee17
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-13 09:26:15 +0300

    u

--- log_vision_events.py
+++ log_vision_events.py
@@ -1,11 +1,29 @@
 from sonar_service import SonarService
+from cloud_service import CloudService
+from camera_service import CameraService
+import simplejson as json
+
 
 class VisionEventsLogger:
 
     def main(self):
         while (True):
             distance = SonarService.getInstance().measure()
-            print(distance)
+            if (distance < 250):
+                camera_service = CameraService()
+                cloud_processor = CloudService()
+                camera_service.take_photo("photo.jpg")
+                vision_response = self.do_vision_post("photo.jpg")
+                json_vision_response = json.loads(vision_response)
+                try:
+                    rows = json_vision_response['responses'][0]['labelAnnotations']
+                except:
+                    print(json_vision_response)
+                    return
+                data = ""
+                for item in rows:
+                    data = data + str(item['description']) + ","
+                print (data)
 
 
 vision_logger = VisionEventsLogger()
