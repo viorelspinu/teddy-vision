f6f215ed7fd8b6f80cf7a3e39f225ce4e2d0ddc3
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-16 10:00:22 +0300

    u

--- remote_webserver/main.py
+++ remote_webserver/main.py
@@ -1,20 +1,6 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+
 
 from __future__ import print_function
-
-# [START gae_flex_websockets_app]
 from flask import Flask, render_template
 from flask_sockets import Sockets
 
@@ -27,15 +13,12 @@ sockets = Sockets(app)
 def chat_socket(ws):
     while not ws.closed:
         message = ws.receive()
-        if message is None:  # message is "None" if the client has closed.
+        if message is None:  
             continue
-        # Send the message to all clients connected to this webserver
-        # process. (To support multiple processes or instances, an
-        # extra-instance storage or messaging system would be required.)
+
         clients = ws.handler.server.clients.values()
         for client in clients:
             client.ws.send(message)
-# [END gae_flex_websockets_app]
 
 
 @app.route('/')
