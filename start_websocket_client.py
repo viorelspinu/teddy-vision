e88d8cefe4c0bda55d9531a4c60f90b4c14e7e69
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-16 10:05:30 +0300

    u

--- start_websocket_client.py
+++ start_websocket_client.py
@@ -1,13 +1,14 @@
 from lomond import WebSocket
+from lomond.persist import persist
 from cloud_service import CloudService
 from voice_codes_constants import *
 
 websocket = WebSocket('http://116.203.129.161:80/chat')
 cloud_service = CloudService()
 
-for event in websocket:
+for event in persist(websocket):
     if event.name == "ready":
-        print ("ready")
+        print("ready")
     elif event.name == "text":
         print(event.text)
         cloud_service.speak(event.text, TTS_LANGUAGE_CODE_ENGLISH)
