ba24cd5c9b360ac55a86c97af4b4e01dc2806ffd
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-16 14:14:05 +0300

    u

--- start_websocket_client.py
+++ start_websocket_client.py
@@ -1,6 +1,7 @@
 from lomond import WebSocket
 from lomond.persist import persist
 from cloud_service import CloudService
+from datetime import datetime
 from voice_codes_constants import *
 
 websocket = WebSocket('http://116.203.129.161:80/chat')
@@ -13,6 +14,8 @@ for event in persist(websocket):
         print(event.text)
         parts = event.text.split("_#_#_")
         cloud_service.speak(parts[1], parts[0])
+        now = datetime.now()
+        date_string = now.strftime("%d/%m/%Y %H:%M:%S")
         with open("remote_speech_log.txt", "a") as myfile:
-            myfile.write(event.text + "\r\n")
+            myfile.write(date_string + ": " + event.text + "\r\n")
             myfile.close()
