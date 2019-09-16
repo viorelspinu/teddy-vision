faa26097966214ab9c734637b0889f90c33e99d3
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-16 10:02:59 +0300

    u

--- start_websocket_client.py
+++ start_websocket_client.py
@@ -2,7 +2,7 @@ from lomond import WebSocket
 from cloud_service import CloudService
 from voice_codes_constants import *
 
-websocket = WebSocket('http://116.203.129.161:8080/chat')
+websocket = WebSocket('http://116.203.129.161:80/chat')
 cloud_service = CloudService()
 
 for event in websocket:
