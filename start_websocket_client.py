52ff602e668648fa0e06be6a81b7f815869cfcd7
Viorel Spinu <viorel.spinu@gmail.com>
2019-09-23 11:24:36 +0300

    u

--- start_websocket_client.py
+++ start_websocket_client.py
@@ -29,6 +29,7 @@ for event in persist(websocket):
         if (text.startswith("__SENSITIVITY__")):
             text = text.replace("__SENSITIVITY__", "")
             json_sensitivity = json.load(text)
+            console.log(json_sensitivity)
             configuration_service.set_sensitivities(json_sensitivity)
             
             
