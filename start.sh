7c520edce4fcb7d455c7e69aa4e3f0b03f24e365
Viorel Spinu <viorel.spinu@gmail.com>
2019-10-10 17:50:53 +0300

    u

--- start.sh
+++ start.sh
@@ -18,18 +18,19 @@ export GOOGLE_API_KEY=__YOUR_GOOGLE_API_KEY___
 #go to https://portal.azure.com 
 export SPEECH_SERVICE_KEY=__YOUR_MICROSOFT_COGNITIVE_SERVICES_API_KEY
 
-aplay ./wav/up_and_running.wav
-python ./configuration_service.py  > /home/pi/configuration_service.log
-#python ./say_ip_address.py
-python ./start_server.py  > /home/pi/start_server.log &
 
-./start_websocket_client.sh  > /home/pi/start_websocket_client.log &
+aplay ./wav/up_and_running.wav
+python ./configuration_service.py  >> /home/pi/configuration_service.log 2>&1
+#python ./say_ip_address.py
+python ./start_server.py  >> /home/pi/start_server.log 2>&1 &
+
+./start_websocket_client.sh  >> /home/pi/start_websocket_client.log 2>&1 &
 
 while true
 do
-python ./wait_for_trigger.py > /home/pi/wait_for_trigger.log
+python ./wait_for_trigger.py >> /home/pi/wait_for_trigger.log 2>&1
 
-python ./run_action_on_hotword.py > /home/pi/run_action_on_hotword.log
+python ./run_action_on_hotword.py >> /home/pi/run_action_on_hotword.log 2>&1
 
 sleep 0.5
 
